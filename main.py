f08db07122d154e33d853f1527e80b24fbbcc4e2
AbdulHaseeb-dev <haseebrana.faisal@gmail.com>
2024-01-24 22:03:31 +0500

    basic function

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 marbles = [10, 13, 39, 14, 41, 9, 3]
 print('The total is', sum(marbles))
+
+def compute_sum(list):
+    sum = 0
+    for number in list:
+       sum = sum + number
+    return sum
+print('The total is', compute_sum(marbles))
